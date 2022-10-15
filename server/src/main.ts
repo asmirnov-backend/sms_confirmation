4ecf889f59fad3592636ffd12b414c849fb9a4a2
Andrew Smirnov <smirnov@brick.dev>
2022-10-15 11:02:44 +0300

    Add cors to server

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,8 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const PORT = 3000;
-  const app = await NestFactory.create(AppModule);
+  const PORT = 3005;
+  const app = await NestFactory.create(AppModule, {
+    cors: { origin: 'http://localhost:3006' },
+  });
   await app.listen(PORT);
 }
 bootstrap();
